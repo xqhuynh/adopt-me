4386d15654437e70fe88e4744fcf35ad4c1627d7
Savon Huynh <xqhuynh@gmail.com>
2022-09-29 08:32:22 -0700

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Pet from "./Pet";
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doik" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   )
 }
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doink",
-//       animal: "Cat",
-//       breed: "Mix",
-//     }),
-//   ]);
-// };
 
 render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -8,14 +8,4 @@ const Pet = (props) => {
     )
 }
 
-// import React from "react";
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//         React.createElement("h2", {}, props.breed),
-//     ]);
-// };
-
 export default Pet;
